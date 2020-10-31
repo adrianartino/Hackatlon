f897399bbb5b80cd6f9fee02b0e553b47428d7fe
unknown <marcosadrian2707@gmail.com>
2020-10-30 20:59:46 -0600

    Bootstrap Agregado

--- index.php
+++ index.php
@@ -3,11 +3,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles/bootstrap/css/bootstrap.min.css">
     <title>Document</title>
 </head>
 <body>
     <?php 
         echo 'Hola mundo cruel';
     ?>
+    <button class="btn btn-secondary">dolor atmet</button>
+
+    
+    <script src="styles/bootstrap/js/jquery-3.4.1.min.js"></script>
+    <script src="styles/bootstrap/js/popper.min.js"></script>
+    <script src="styles/bootstrap/js/bootstrap.min.js"></script>
 </body>
 </html>
