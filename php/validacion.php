531e33325245566c913b927c5553b99579c2c43d
unknown <marcosadrian2707@gmail.com>
2020-11-03 02:15:50 -0600

    Se agregaron elementos graficos

---
+++ php/validacion.php
@@ -0,0 +1,49 @@
+
+<?php 
+
+if(isset($_POST)){
+    //variables equipo
+    $equipo = $_POST['equipo'];
+    $institucion = $_POST['ins'];
+
+    //variables lider
+    $nombreLider = $_POST['nomlid'];
+    $apellidoLider = $_POST['aplid'];
+    $telefono = $_POST['tellid'];
+    $correo = $_POST['colid'];
+    $carreraLider = $_POST['calid'];
+
+    //variables segundo integrante
+    $nomsegundo = $_POST['nom2'];
+    $apsegundo = $_POST['ap2'];
+    $casegundo = $_POST['ca2'];
+
+    //variables tercer integrante
+    $nomtercero = $_POST['nom2'];
+    $aptercero = $_POST['ap2'];
+    $catercero = $_POST['ca2'];
+
+    //variables cuarto integrante
+    $nomcuarto = $_POST['nom2'];
+    $apcuarto = $_POST['ap2'];
+    $cacuarto = $_POST['ca2'];
+
+    //Arreglo para errores
+    $errores = "";
+
+    if ($equipo == ""){
+        $errores = "No ha ingresado el nombre";
+    }
+
+    if ($errores ==""){
+        echo "Se ingreso el nombre";
+    }
+    else{
+        echo "No ha ingresado el nombre xd";
+    }
+
+    
+
+}
+
+?>
