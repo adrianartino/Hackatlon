39f9170a596da1f4c37a130881abf0554322b003
unknown <marcosadrian2707@gmail.com>
2020-11-08 17:40:42 -0600

    Se agregó el correo de hackaton

--- php/validacion.php
+++ php/validacion.php
@@ -208,14 +208,14 @@
                 $mail->isSMTP();                                            // Send using SMTP
                 $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
                 $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-                $mail->Username   = 'marcosadrian2707@gmail.com';                     // SMTP username
-                $mail->Password   = 'marcos619';                               // SMTP password
+                $mail->Username   = 'hackaton@lerdo.tecnm.mx';                     // SMTP username
+                $mail->Password   = 'Hackaanmp!21';                               // SMTP password
                 $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
                 $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
     
                 //Recipients
-                $mail->setFrom('marcosadrian2707@gmail.com', 'Hackaton');
-                $mail->addAddress($correo, 'Remitente');     // Add a recipient
+                $mail->setFrom('hackaton@lerdo.tecnm.mx', 'Hackaton');
+                $mail->addAddress($correo, $equipo);     // Add a recipient
     
                 // Attachments
                 $archivo = 'manual.pdf';
